bc55d7ad1a18b0d6e88b57f6d3c3514ee5d42d0d
AVendingMachine <79037156+AVendingMachine@users.noreply.github.com>
2025-10-14 00:17:58 -0400

    Initializes database automatically when there isnt one

--- src/functions/database-methods.ts
+++ src/functions/database-methods.ts
@@ -1,37 +1,65 @@
 import Database from '@tauri-apps/plugin-sql'
 
-/**
- * Loads the main.db SQLite database stored in appdata/roaming
- * and returns the loaded database
- */
-export async function loadMainDatabase(): Promise<Database> {
-    return await Database.load("sqlite:test.db")
+interface Author {
+    name: string
+    nationality: string
 }
 
+
 /**
- * Adds an author with the specified name to the given database
- * @param db opened SQLite database with an authors table
- * @param name the name of the author to be inserted
+ * Loads the main.db SQLite database stored in appdata/roaming,
+ * creates one if it doesn't exit already
+ * @returns the loaded database object
  */
-export function addAuthor(db: Database, name: string): void {
-    db.execute("INSERT INTO authors (name) VALUES (?)", [name])
+export async function loadMainDatabase(): Promise<Database> {
+    const db = await Database.load("sqlite:main.db")
+    await db.execute(`CREATE TABLE IF NOT EXISTS authors
+                      (
+                          id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                          name        TEXT NOT NULL,
+                          nationality TEXT NOT NULL
+                      )
+    `)
+    await db.execute(`CREATE TABLE IF NOT EXISTS works
+                      (
+                          id        INTEGER PRIMARY KEY AUTOINCREMENT,
+                          name      TEXT    NOT NULL,
+                          genre     TEXT    NOT NULL,
+                          author_id INTEGER NOT NULL,
+                          FOREIGN KEY (author_id) REFERENCES authors (id) ON DELETE CASCADE
+                      )
+    `)
+    return db
 }
 
 /**
  * Gets the name of a provided author by ID in provided database
  * @param db opened SQLite database with an authors table
  * @param id the ID (primary key) of desired author
+ * @returns name of the author
  */
 export async function getAuthorNameByID(db: Database, id: number): Promise<string> {
     const selectedArray: { name: string }[] = await db.select("SELECT name FROM authors WHERE id = ?", [id])
     return selectedArray[0].name //Since the above returns an array with one object, we take that object's name
 }
 
-export async function getTableByName(db: Database, name: string): Promise<{
-    id: number,
-    name: string,
-    nationality: string
-}[]> {
+/**
+ * Returns an array of objects from a given database name
+ * @param db database to open
+ * @param name name of the table within the database
+ * @returns an array of object within the table
+ */
+export async function getTableByName(db: Database, name: string): Promise<{}[]> {
     return await db.select(`SELECT *
                             FROM "${name}"`)
+}
+
+/**
+ * Adds an author object specified to the given database's authors table
+ * @param db the database to insert into
+ * @param author the author object to be inserted
+ */
+export async function addAuthor(db: Database, author: Author): Promise<void> {
+    await db.execute(`INSERT INTO authors (name, nationality)
+                      VALUES ("${author.name}", "${author.nationality}")`)
 }
